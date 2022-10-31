32b322dd40b0b91caf64b342ba7b7d1b14bda376
laurenmcatino <laurenmcatino@gmail.com>
2022-10-31 13:06:19 -0600

    HW 19 JS update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,46 @@
 console.log("your index.js file is loaded correctly!");
 
 $("#resumeButton").on("click", function(){
-    $(this).html("Check your Downloads")
-})
+    $(this).html("Resume Downloaded!");
+}
+);
+
+$(".wiyf").hover(
+    function(){
+        $(this).css("background-color", "orange");
+        $(this).css("color", "rgb(160, 60, 60)");
+    }, function() {
+        $(this).css("background-color", "teal");
+        $(this).css("color", "white");
+    }
+);
+
+$(".globetrot").hover(
+    function(){
+        $(this).css("background-color", "orange");
+        $(this).css("color", "rgb(160, 60, 60)");
+    }, function() {
+        $(this).css("background-color", "teal");
+        $(this).css("color", "white");
+    }
+);
+
+$(".deptofenergy").hover(
+    function(){
+        $(this).css("background-color", "orange");
+        $(this).css("color", "rgb(160, 60, 60)");
+    }, function() {
+        $(this).css("background-color", "teal");
+        $(this).css("color", "white");
+    }
+);
+
+$(".plannedPethood").hover(
+    function(){
+        $(this).css("background-color", "orange");
+        $(this).css("color", "rgb(160, 60, 60)");
+    }, function() {
+        $(this).css("background-color", "teal");
+        $(this).css("color", "white");
+    }
+);
